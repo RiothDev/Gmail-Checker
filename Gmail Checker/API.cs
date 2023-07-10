0505dbf91c6ec7b141ebd54469c84190594e1342
HazuDev <justanotheralt102@gmail.com>
2023-07-10 17:35:46 -0600

    first

---
+++ Gmail Checker/API.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+
+namespace App
+{
+    internal class EmailAPI
+    {
+        public static async Task<bool> SendRequest(string email)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync($"https://mail.google.com/mail/gxlu?email={email}");
+
+                    foreach (var header in response.Headers) if (Convert.ToString(header.Key) == "Set-Cookie") return false;
+
+                    return true;
+
+                } catch(Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static string Check(bool state) {
+            return state ? "Available" : "Not available";
+        }
+    }
+}
---
+++ Gmail Checker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class Program
+    {
+        static async Task _Init()
+        {
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("> Email: ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string? email = Console.ReadLine();
+
+            if (email != null)
+            {
+                bool result = await EmailAPI.SendRequest(email);
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Email status: {EmailAPI.Check(result)}");
+            }
+
+            Console.ResetColor();
+
+            Console.ReadKey();
+            await _Init();
+        }
+
+        static async Task Main(string[] args) => await _Init();
+    }
+}
